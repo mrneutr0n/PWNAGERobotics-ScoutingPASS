b67e3705d284136f5dfa899369174a8ff13452c1
mrneutr0n <arthurname@gmail.com>
2026-04-01 11:55:29 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface functions to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "DEorvWT7kT9UKTMyFOpUvHpVvW6YjILaVqw6duGiRoMMr8K0AYJ2wIFHSHKELZzD";
 /**
  * Get list of teams in event
  *
